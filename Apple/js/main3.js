65f4ca79f0bd097dad6b5428ba2015b2bcad6186
seirblue <rhkrehs21@gmail.com>
2023-10-06 17:57:13 +0900

    apple

--- Apple/js/main3.js
+++ Apple/js/main3.js
@@ -1,4 +1,15 @@
-$(function(){
-    //상단 메뉴 슬라이드
-    
-})
+$(function () {
+  //상단 메뉴 슬라이드
+
+  var topmenu = $(".top-menu ul li");
+  var submenu = $(".sub-menu");
+
+  $(topmenu).hover(
+    function () {
+        $(submenu).slideDown();
+    },
+    function () {
+        $(submenu).slideUp();
+    }
+  );
+});
