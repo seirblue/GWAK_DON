82b44bb93eae799df2a23d1876c11338bc794129
seirblue <rhkrehs21@gmail.com>
2023-10-10 17:16:15 +0900

    apple

--- Apple/js/main3.js
+++ Apple/js/main3.js
@@ -9,7 +9,7 @@ $(function () {
   $(topmenu).hover(
     function () {
       let i = $(this).index();
-        $(submenu).eq(i).stop().slideDown(500,'linear');
+        $(submenu).eq(i).stop().slideDown(500);
         $(".top-menu").css({
           background:"#000",
           transition:"0.2s",
@@ -17,16 +17,16 @@ $(function () {
         /* $(submenu).animate(500,'linear'); */
         $(submenu).find(lnb).css({
           opacity:"0",
-          transition:"0.3s"
+          transition:"0.2s"
         })
         $(submenu).eq(i).find(lnb).css({
           opacity:"1",
-          transition:"0.3s"
+          transition:"0.2s"
         })
         $(wrap).stop().addClass("on")
     },
     function () {
-        $(submenu).stop().slideUp(200,'linear');
+        $(submenu).stop().slideUp(1000);
         /* $(submenu).animate(500,'linear'); */
         /* $(wrap).removeClass("on") */
     }
@@ -35,7 +35,7 @@ $(function () {
     $(this).stop().slideDown(500,'linear');
     /* $(wrap).addClass("on") */
   },function(){
-    $(this).stop().slideUp(200,'linear');
+    $(this).stop().slideUp(200);
         $(".top-menu").css({
           background:"rgba(22,22,25,0.8)",
           transitionDelay:"0.5s",
