4d09db87b19bd419d5fd6929193bfffb61512293
seirblue <rhkrehs21@gmail.com>
2023-10-10 15:39:30 +0900

    apple

--- Apple/js/main3.js
+++ Apple/js/main3.js
@@ -1,15 +1,45 @@
 $(function () {
   //상단 메뉴 슬라이드
 
-  var topmenu = $(".top-menu ul li");
-  var submenu = $(".sub-menu");
+  let topmenu = $(".top-menu ul li");
+  let submenu = $(".sub-menu");
+  let lnb = $(".sub-menu .lnb");
+  let wrap = $("#wrap");
 
   $(topmenu).hover(
     function () {
-        $(submenu).slideDown();
+      let i = $(this).index();
+        $(submenu).eq(i).stop().slideDown(500,'linear');
+        $(".top-menu").css({
+          background:"#000",
+          transition:"0.2s",
+        })
+        /* $(submenu).animate(500,'linear'); */
+        $(submenu).find(lnb).css({
+          opacity:"0",
+          transition:"0.3s"
+        })
+        $(submenu).eq(i).find(lnb).css({
+          opacity:"1",
+          transition:"0.3s"
+        })
+        $(wrap).stop().addClass("on")
     },
     function () {
-        $(submenu).slideUp();
+        $(submenu).stop().slideUp(200,'linear');
+        /* $(submenu).animate(500,'linear'); */
+        /* $(wrap).removeClass("on") */
     }
   );
+  $(submenu).hover(function(){
+    $(this).stop().slideDown(500,'linear');
+    /* $(wrap).addClass("on") */
+  },function(){
+    $(this).stop().slideUp(200,'linear');
+        $(".top-menu").css({
+          background:"rgba(22,22,25,0.8)",
+          transitionDelay:"0.5s",
+        })
+    $(wrap).stop().removeClass("on")
+  })
 });
